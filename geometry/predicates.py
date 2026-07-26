1dc7919205881105c7c0d56dd2300c0ad4bf9d90
Devadath2004 <devadathrs2004@gmail.com>
2026-07-26 15:49:55 +0530

    add in_circumcircle predicate

--- geometry/predicates.py
+++ geometry/predicates.py
@@ -6,3 +6,17 @@ def orientation(a,b,c):
         return -1
     else:
         return 0
+def in_circumcircle(a,b,c,d):
+    ax, ay = a.x - d.x, a.y - d.y
+    bx, by = b.x - d.x, b.y - d.y
+    cx, cy = c.x - d.x, c.y - d.y
+    ax2ay2 = ax*ax +ay*ay
+    bx2by2 = bx*bx +by*by
+    cx2cy2 = cx*cx +cy*cy
+
+    det = (ax * (by * cx2cy2 - bx2by2 * cy)
+           - ay * (bx * cx2cy2 - bx2by2 * cx)
+           + ax2ay2 * (bx * cy - by * cx))
+
+    return det > 0
+
