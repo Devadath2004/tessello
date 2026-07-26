f8669d8385e683004eba3e628b7ed4bc3142e622
Devadath2004 <devadathrs2004@gmail.com>
2026-07-26 18:55:27 +0530

    Add find_bad_triangles

--- triangulate/delaunay.py
+++ triangulate/delaunay.py
@@ -1,5 +1,5 @@
 from geometry.primitives import Point, Triangle
-
+from geometry.predicates import orientation, in_circumcircle
 def super_triangle(points):
     min_x = min(p.x for p in points)
     max_x = max(p.x for p in points)
@@ -15,3 +15,18 @@ def super_triangle(points):
     p3 = Point((min_x +max_x)/2, max_y+delta)
 
     return p1,p2,p3
+def find_bad_triangles(triangles, points, new_point):
+    """
+    triangles : list of objects (each storing 3 point *indices*)
+    points : the master list of Point objects that the triangle's indices refer to
+    new_point : the Point currently being inserted
+    Returns : list of triangle objects whose circumcircle contains new point
+    """
+    bad =[]
+    for tri in triangles:
+        a = points[tri.a]
+        b = points[tri.b]
+        c = points[tri.c]
+        if in_circumcircle(a,b,c,new_point):
+            bad.append(tri)
+    return bad
